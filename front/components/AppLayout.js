06c7421e70bc02d7425b7cda3b2c4dd2b64c803d
overloper <overloper0225@gmail.com>
2023-04-30 18:39:35 +0900

    Responsive grid

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import propsTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Input, Menu, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
@@ -14,10 +14,23 @@ const AppLayout = ({ children }) => {
           <Link href="/profile">프로필</Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">회원가입</Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          오른쪽 메뉴
+        </Col>
+      </Row>
     </div>
   );
 };
