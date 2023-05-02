4b3a9c38f455f97c84e68d5ac4d8ba90bb1b826b
overloper <overloper0225@gmail.com>
2023-05-03 08:24:22 +0900

    Create redux store

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import propTypes from "prop-types";
 import Head from 'next/head';
+import wrapper from "../store/configureStore";
 
 const NodeBird = ({ Component }) => {
   return (
@@ -18,4 +19,4 @@ NodeBird.propTypes = {
   Component: propTypes.elementType.isRequired,
 };
 
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);
