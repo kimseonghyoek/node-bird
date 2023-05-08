49e5a8cc42ca4970504cf04c8de30cc439d97045
overloper <overloper0225@gmail.com>
2023-05-08 16:47:03 +0900

    Update toggle logic

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {Avatar, Button, Card, Image, Popover} from "antd";
-import {EllipsisOutlined, HeartOutlined, MessageOutlined, RetweetOutlined} from "@ant-design/icons";
+import {EllipsisOutlined, HeartOutlined, HeartTwoTone, MessageOutlined, RetweetOutlined} from "@ant-design/icons";
 import {useSelector} from "react-redux";
 import PostImages from "./PostImages";
 const PostCard = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, []);
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened((prev) => !prev);
+  }, []);
+
   const id = useSelector((state) => state.user.me?.id);
     return (
         <div style={{ marginBottom: 20}}>
@@ -11,8 +20,8 @@ const PostCard = ({ post }) => {
                 cover={post.Images[0] && <PostImages images={post.Images} />}
                 actions={[
                   <RetweetOutlined key="retweet"/>,
-                  <HeartOutlined key="heart"/>,
-                  <MessageOutlined key="comment"/>,
+                  liked ? <HeartTwoTone twoToneColor="#eb2f96" onClick={onToggleLike} key="heart"/> : <HeartOutlined onClick={onToggleLike} key="heart"/>,
+                  <MessageOutlined key="comment" onClick={onToggleComment}/>,
                   <Popover key="more" content={(
                     <Button.Group>
                       { id && post.User.id === id ? (
@@ -34,6 +43,9 @@ const PostCard = ({ post }) => {
                   description={post.content}
                   />
             </Card>
+          {commentFormOpened && (<div>
+            댓글 부분
+          </div>)}
             {/*<CommentForm/>*/}
             {/*<Comments/>*/}
         </div>
