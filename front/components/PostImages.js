b4af05129f4f40ae7ce5ea34a079e4f4bc938c77
overloper <overloper0225@gmail.com>
2023-05-10 21:29:17 +0900

    ImageZoom udpate

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,17 +1,24 @@
 import React, {useCallback, useState} from 'react';
 import PropTypes from "prop-types";
 import {PlusOutlined} from "@ant-design/icons";
+import ImagesZoom from "./imagesZoom";
 
 const PostImages = ({ images }) => {
   const [showImageZoom, setShowImagesZoom] = useState(false);
 
   const onZoom = useCallback(() => {
     setShowImagesZoom(true);
-  }, [])
+  }, []);
+
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  })
+
   if (images.length === 1) {
     return (
       <div style={{ display: 'flex', justifyContent: 'center'}}>
         <img role="presentation" style={{ maxWidth: '50%', textAlign: 'center'}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        { showImageZoom && <ImagesZoom images={images} onClose={onClose} /> }
       </div>
     )
   }
@@ -20,6 +27,7 @@ const PostImages = ({ images }) => {
       <>
         <img role="presentation" style={{ width: '50%', display: 'inline-block'}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
         <img role="presentation" style={{ width: '50%', display: 'inline-block'}} src={images[1].src} alt={images[0].src} onClick={onZoom} />
+        { showImageZoom && <ImagesZoom images={images} onClose={onClose} /> }
       </>
     )
   }
@@ -34,6 +42,7 @@ const PostImages = ({ images }) => {
           개의 사진 더보기
         </div>
       </div>
+      { showImageZoom && <ImagesZoom images={images} onClose={onClose} /> }
     </>
   )
 }
