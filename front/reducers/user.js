76ace0bf3cfb770d50110c243144d3a5d71d214f
overloper <overloper0225@gmail.com>
2023-06-22 23:36:15 +0900

    Update reducers

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,89 +1,145 @@
 export const initalState = {
-  isLoggingIn: false, // 로그인 시도 중
-  isLoggedIn: false,
-  isLoggingOut: false, // 로그아웃 시도 중
+  logInLoading: false, // 로그인 시도 중
+  logInDone: false,
+  logInError: null,
+  logOutLoading: false, // 로그아웃 시도 중
+  logOutDone: false,
+  logOutError: null,
+  signUpLoading: false, // 회원가입 시도 중
+  signUpDone: false,
+  signUpError: null,
   me: null,
   signUpData: {},
   loginData: {},
 };
 
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST";
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
+
+export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
+
+export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
+
+export const FOLLOW_REQUREST = "FOLLOW_REQUEST";
+export const FOLLOW_SUCCESS = "FOLLOW_SUCCESS";
+export const FOLLOW_FAILURE = "FOLLOW_FAILURE";
+
 export const loginRequestAction = (data) => {
   return {
-    type: "LOG_IN_REQUEST",
-    data
+    type: LOG_IN_REQUEST,
+    data,
   };
 };
 
-export const loginSuccessAFction = (data) => {
+export const loginSuccessAction = (data) => {
   return {
-    type: "LOG_IN_SUCCESS",
-    data
+    type: LOG_IN_SUCCESS,
+    data,
   };
 };
 
 export const loginFailureAction = (data) => {
   return {
-    type: "LOG_IN_FAILURE",
-    data
+    type: LOG_IN_FAILURE,
+    data,
   };
 };
 
 export const logoutRequestAction = () => {
   return {
-    type: "LOG_OUT_REQUEST",
+    type: LOG_OUT_REQUEST,
   };
 };
 
 export const logoutSuccessAction = () => {
   return {
-    type: "LOG_OUT_SUCCESS",
+    type: LOG_OUT_SUCCESS,
   };
 };
 
 export const logoutFailureAction = () => {
   return {
-    type: "LOG_OUT_FAILURE",
+    type: LOG_OUT_FAILURE,
   };
 };
 
+const dummyUser = (data) => ({
+  ...data,
+  nickname: "Overloper",
+  id: 1,
+  Posts: [],
+  Followings: [],
+  follwers: [],
+});
+
 const reducer = (state = initalState, action) => {
   switch (action.type) {
-    case "LOG_IN_REQUEST":
+    case LOG_IN_REQUEST:
       return {
         ...state,
-        isLoggingIn: true,
+        logInLoading: true,
+        logInError: null,
+        logInDone: false,
         me: action.data,
       };
-    case "LOG_IN_SUCCESS":
+    case LOG_IN_SUCCESS:
       return {
         ...state,
-        isLoggingIn: false,
-        isLoggedIn: true,
-        me: { ...action.data, nickname: 'Overloper' }
-      }
-    case "LOG_IN_FAILURE":
-      return {
-        ...state,
-        isLoggingIn: false,
-        isLoggedIn: false,
-      }
-    case "LOG_OUT_REQUEST":
-      return {
-        ...state,
-        isLoggingOut: true,
+        logInLoading: false,
+        logInDone: true,
+        me: dummyUser(action.data),
       };
-    case "LOG_OUT_SUCCESS":
+    case LOG_IN_FAILURE:
       return {
         ...state,
-        isLoggedIn: false,
-        isLoggingOut: false,
+        logInLoading: false,
+        logInError: action.error,
+      };
+    case LOG_OUT_REQUEST:
+      return {
+        ...state,
+        logOutLoading: true,
+        logOutDone: false,
+        logOutError: null,
+      };
+    case LOG_OUT_SUCCESS:
+      return {
+        ...state,
+        logOutLoading: false,
+        logOutDone: true,
         me: null,
-      }
-    case "LOG_OUT_FAILURE":
+      };
+    case LOG_OUT_FAILURE:
       return {
         ...state,
-        isLoggingOut: false,
-      }
+        logOutLoading: false,
+        logOutError: action.error,
+      };
+      case SIGN_UP_REQUEST:
+        return {
+          ...state,
+          signUpLoading: true,
+          signUpDone: false,
+          signUpError: null,
+        };
+      case SIGN_UP_SUCCESS:
+        return {
+          ...state,
+          signUpLoading: false,
+          signUpDone: true,
+          me: null,
+        };
+      case SIGN_UP_FAILURE:
+        return {
+          ...state,
+          signUpLoading: false,
+          signUpError: action.error,
+        };
     default:
       return state;
   }
