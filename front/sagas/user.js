c39e47ef933c9f0cf7ed8efcef3c2703912ce31f
overloper <overloper0225@gmail.com>
2023-08-02 16:21:10 +0900

    Update

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,6 +1,6 @@
 import { all, fork, put, delay, takeLatest } from 'redux-saga/effects';
 import axios from "axios";
-import { LOG_IN_FAILURE, LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOG_OUT_SUCCESS, SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS } from '../reducers/user';
+import { FOLLOW_FAILURE, FOLLOW_REQUREST, FOLLOW_SUCCESS, LOG_IN_FAILURE, LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOG_OUT_SUCCESS, SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS, UNFOLLOW_FAILURE, UNFOLLOW_REQUREST, UNFOLLOW_SUCCESS } from '../reducers/user';
 
 function loginAPI(data) {
   return axios.post("/api/login", data)
@@ -65,10 +65,60 @@ function* signUp() {
       error: err.response.data
     })
   }
+};
+
+function followAPI() {
+  return axios.post('/api/follow');
+}
+
+function* follow(action) {
+  try {
+    // const result = yield call(followAPI);
+    yield delay(1000);
+    yield put({
+      type: FOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: FOLLOW_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function unfollowAPI() {
+  return axios.post('/api/unfollow');
+}
+
+function* unfollow(action) {
+  try {
+    // const result = yield call(unfollowAPI);
+    yield delay(1000);
+    yield put({
+      type: UNFOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: UNFOLLOW_FAILURE,
+      error: err.response.data,
+    });
+  }
 }
 
 function* watchSignUp() {
   yield takeLatest(SIGN_UP_REQUEST, signUp)
+}
+
+function* watchFollow() {
+  yield takeLatest(FOLLOW_REQUREST, follow);
+}
+
+function* watchUnfollow() {
+  yield takeLatest(UNFOLLOW_REQUREST, unfollow);
 }
 
 export default function* userSaga() {
@@ -76,5 +126,7 @@ export default function* userSaga() {
     fork(watchLogin),
     fork(watchLogOut),
     fork(watchSignUp),
+    fork(watchFollow),
+    fork(watchUnfollow),
   ]);
 }
