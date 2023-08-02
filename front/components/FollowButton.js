b362b8faddb25f40aafbfcae550b99c741a85629
overloper <overloper0225@gmail.com>
2023-08-02 16:20:47 +0900

    Create FollowBtn

---
+++ front/components/FollowButton.js
@@ -0,0 +1,36 @@
+import React, { useCallback } from 'react';
+import { Button } from 'antd';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { FOLLOW_REQUEST, FOLLOW_REQUREST, UNFOLLOW_REQUEST, UNFOLLOW_REQUREST } from '../reducers/user';
+
+const FollowButton = ({ post }) => {
+  const dispatch = useDispatch();
+  const { me, followLoading, unfollowLoading } = useSelector((state) => state.user);
+  const isFollowing = me?.Followings.find((v) => v.id === post.User.id);
+  const onClickButton = useCallback(() => {
+    if (isFollowing) {
+      dispatch({
+        type: UNFOLLOW_REQUREST,
+        data: post.User.id,
+      });
+    } else {
+      dispatch({
+        type: FOLLOW_REQUREST,
+        data: post.User.id,
+      });
+    }
+  }, [isFollowing]);
+
+  return (
+    <Button loading={followLoading || unfollowLoading} onClick={onClickButton}>
+      {isFollowing ? '언팔로우' : '팔로우'}
+    </Button>
+  );
+};
+
+FollowButton.propTypes = {
+  post: PropTypes.object.isRequired,
+};
+
+export default FollowButton;
