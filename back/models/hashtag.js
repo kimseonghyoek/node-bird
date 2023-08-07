3b808f46828d73c557414ab3c0a3f5e0d3b9048f
overloper <overloper0225@gmail.com>
2023-08-07 21:40:45 +0900

    Add Model

---
+++ back/models/hashtag.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Hashtag = sequelize.define('Hashtag', {
+    name: {
+      type: DataTypes.STRING(20),
+      allowNull: true,
+    },
+  }, {
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
+  });
+  Hashtag.associate = (db) => {};
+  return Hashtag;
+}
