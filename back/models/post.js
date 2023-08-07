cbd47c253403626366c05306cc84a8ec75f9b008
overloper <overloper0225@gmail.com>
2023-08-08 01:38:14 +0900

    Update

--- back/models/post.js
+++ back/models/post.js
@@ -8,6 +8,13 @@ module.exports = (sequelize, DataTypes) => {
     charset: 'utf8mb4',
     collate: 'utf8mb4_general_ci',
   });
-  Post.associate = (db) => {};
+  Post.associate = (db) => {
+    db.Post.belongsTo(db.User);
+    db.Post.belongsToMany(db.Hashtag);
+    db.Post.hasMany(db.Comment);
+    db.Post.hasMany(db.Image);
+    db.Post.belongsTo(db.Post, { as: { 'Retweet' }});
+    db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' });
+  };
   return Post
 }
--- back/models/user.js
+++ back/models/user.js
@@ -10,13 +10,20 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
     password: {
-      type: DataTypes.STRING(30),
+      type: DataTypes.STRING(100),
       allowNull: false,
     }
   }, {
     charset: 'utf8',
     collate: 'utf8_general_ci',
   });
-  User.associate = (db) => {};
+  User.associate = (db) => {
+    db.User.hasMany(db.Post);
+    db.User.hasMany(db.Comment);
+    db.User.belongsToMany(db.Post);
+    db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked'});
+    db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followers', foreignKey: 'followingId' });
+    db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followings', foreignKey: 'followerId' });
+  };
   return User
 }
